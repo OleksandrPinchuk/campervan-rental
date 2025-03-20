096e8d35cdf68e13d200a6297956c3119296e0b1
OleksandrPinchuk <116524115+OleksandrPinchuk@users.noreply.github.com>
2025-03-20 22:55:11 +0100

    styled form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,18 +1,21 @@
 import Button from "../Button/Button";
 import css from "./Form.module.css";
 export const Form = () => {
-    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(event);
+    };
     
     return (
-        <div className={css.form}>
-            <form  className="form-container">
-                <h2>Book your campervan now</h2>
-                <p>Stay connected! We are always ready to help you.</p>
-                <input  placeholder="Name*" />
-                <input placeholder="Email*" type="email" />
-                <input placeholder="Booking date*" type="date" />
-                <textarea placeholder="Comment" />
-                <Button>Send</Button>
+        <div className={css.border}>
+            <form className={css.form}>
+                <h3 className={css.title}>Book your campervan now</h3>
+                <p className={css.text}>Stay connected! We are always ready to help you.</p>
+                <input type="text" name="name" placeholder="Name*" className={css.input} />
+                <input type="email" name="email" placeholder="Email*" className={css.input} />
+                <input placeholder="Booking date*" type="date" className={css.input} />
+                <textarea placeholder="Comment" className={css.textarea} />
+                <Button onClick={handleSubmit}>Send</Button>
             </form>
         </div>
     )
